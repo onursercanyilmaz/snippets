e1f6f663dd44445282b9d27f1d1ab86d72b903e6
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2020-03-18 01:12:05 +0300

    Create HtmlAgilityPack_Example.cs

---
+++ retrieve_data/HtmlAgilityPack_Example.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net;
+using HtmlAgilityPack;
+
+// Boş Sayfa öğe şablonu https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x41f adresinde açıklanmaktadır
+
+namespace HtmlAgilityPack_Example
+{
+    /// <summary>
+    /// Kendi başına kullanılabilecek ya da bir Çerçeve içine taşınabilecek boş bir sayfa.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, RoutedEventArgs e)
+        {
+            Uri url = new Uri("https://izu.edu.tr");
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            HtmlNodeCollection titles = doc.DocumentNode.SelectNodes("//a");
+
+            foreach(var title in titles)
+            {
+                string link = title.Attributes["href"].Value;
+                listBox1.Items.Add(title.InnerText);
+            }
+        }
+    }
+}
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net;
+using HtmlAgilityPack;
+
+// Boş Sayfa öğe şablonu https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x41f adresinde açıklanmaktadır
+
+namespace HtmlAgilityPack_Example
+{
+    /// <summary>
+    /// Kendi başına kullanılabilecek ya da bir Çerçeve içine taşınabilecek boş bir sayfa.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, RoutedEventArgs e)
+        {
+            Uri url = new Uri("https://izu.edu.tr");
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            HtmlNodeCollection titles = doc.DocumentNode.SelectNodes("//a");
+
+            foreach(var title in titles)
+            {
+                string link = title.Attributes["href"].Value;
+                listBox1.Items.Add(title.InnerText);
+            }
+        }
+    }
+}
