14213969dfbaf90edb7f7d409904291e168bfd6e
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2020-04-25 16:55:24 +0300

    Add files via upload

---
+++ CPP/Override_Inheritance.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+class Human
+{
+public:
+    void eat() {cout << "I want to eat SOMETHING" << endl; }
+    void eat(string msg) { cout << " " << msg << endl; }
+};
+
+class Student :public Human
+{
+public:
+    void eat() { cout << "I want to eat MEAT" << endl; }
+    void eat(string msg) { cout << " " << msg << endl; }
+};
+
+class Employee : public Human
+{
+public:
+    void eat() { cout << "I want to eat FISH" << endl; }
+  //  void eat(string msg){ cout << " " << msg << endl; }
+};
+int main()
+{
+    Student std;
+    Employee emp;
+
+    std.Human::eat(); //I want to eat SOMETHING
+    std.eat(); //I want to eat MEAT
+    std.eat("I changed my mind and want to eat CHICKEN"); //I changed my mind and want to eat CHICKEN
+
+    cout << endl;
+
+    emp.Human::eat(); //I want to eat SOMETHING
+    emp.eat();  //"I want to eat FISH"
+   // emp.eat("I would like to eat FISH and drink WINE"); //I would like to eat FISH and drink WINE
+    emp.Human::eat("I would like to eat FISH and drink WINE"); //Or we can use from Paretn class without writing the code which is before 1 line in child class 
+  
+}
+
