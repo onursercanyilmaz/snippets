9274d45095130459fc7c2950189431b8823268e0
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2020-04-25 19:21:49 +0300

    Add files via upload

---
+++ CPP/Inheritance_Shape_Overloading.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Shape
+{
+public:
+    Shape()
+    {
+        cout << "Shape is created." << endl;
+    }
+
+    Shape(int a)
+    {
+        cout << "Overloaded Shape is created." << endl;
+    }
+};
+
+class Rectangle : public Shape
+{
+    public:
+    
+        Rectangle()
+        {
+            cout << "Rectangle is created." << endl;
+        }
+        Rectangle(int a) : Shape(a)
+        {
+            cout << "Overloaded Rectangle is created." << endl;
+        }
+};
+
+class Triangle : public Shape
+{
+    public:
+
+       Triangle()
+       {
+          cout << "Triangle is created." << endl;    
+       }
+       Triangle(int a) : Shape(a)
+       {
+           cout << "Overloaded Triangle is created." << endl;
+       }
+};
+
+
+int main()
+{
+    
+    Rectangle rec1; 
+    Triangle tr1;
+    cout << endl;
+    Rectangle rec2(10);
+    Triangle tr2(10);
+    
+  
+}
+
+
+
+
