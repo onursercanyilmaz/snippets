c7f3294ca4d6ff1cd710295e7a3cdd8bfb849385
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2020-08-09 14:36:00 +0300

    Add files via upload

---
+++ CPP/EuclideanAlgorithm.cpp
@@ -0,0 +1,31 @@
+﻿//Finding Greatest Common Divisor by euclide algorithm
+
+#include<iostream>
+
+using namespace std;
+
+int euclide(int a, int b)
+{
+	int r = 0;
+
+	if (a == 0)
+	{
+		return b;
+	}
+	else if (b == 0)
+	{
+		return a;
+	}
+	else
+	{
+		r = (a % b);
+		return euclide(b, r);
+	}
+}
+
+int main()
+{
+
+	cout << euclide(62, 45) << endl;
+
+}
