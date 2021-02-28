42beb7148b8f1f49f6152567e8c5470a7aacf457
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2021-02-28 14:47:44 +0300

    Create degiskenler.dart

---
+++ Flutter/2021/degiskenler.dart
@@ -0,0 +1,13 @@
+void main() {
+
+String mesaj = "Selam Onur";
+int bday = 1990;
+double oran = 1.50;
+bool isOn = false;
+
+	print(mesaj);
+  //print("Dogum yili: "+ bday); wrong
+  print("Dogum yili: "+ bday.toString()); 
+ 
+  print("Oran : ${oran}");
+}
