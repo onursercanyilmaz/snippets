152776a21b33f670ca53a95d6adadca1cc26c460
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2021-03-01 10:26:54 +0300

    Create loops.dart

---
+++ Flutter/2021/loops.dart
@@ -0,0 +1,40 @@
+void main() {
+  
+for(var i = 1; i<=10;i++)
+{
+print(i);
+}
+//-------
+var product = ["A","B","C"];
+
+for(var i = 0; i<product.length; i++)
+{
+print(product[i]);
+
+}
+
+for(var prod in product)
+{
+print(prod);
+}
+
+//while---------
+int sayi = 10;
+while(sayi >= 0)
+{
+print(sayi);
+sayi--;
+}
+
+//do-while-------
+var sayi2 = 100;
+do
+{
+print("Sayı : ${sayi2}");
+sayi++;
+}while(sayi>100);
+
+
+
+
+}
