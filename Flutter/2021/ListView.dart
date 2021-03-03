695f009e441060e0216c777b1b74ab2d82e647ee
Onur Sercan <57360358+onursercanyilmaz@users.noreply.github.com>
2021-03-03 22:40:44 +0300

    Update ListView.dart

--- Flutter/2021/ListView.dart
+++ Flutter/2021/ListView.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/student.dart';
+
 void main() {
   runApp(MaterialApp(home: MyApp()));
 }
@@ -10,58 +12,96 @@ void main() {
 
 //my widget
 class MyApp extends StatelessWidget {
-  String mesaj = "İlk Uygulamam!";
-  var students = [
-    "Onur Sercan",
+  String mesaj = "Result Page";
+  List<Student> students = [
+    Student("Onur Sercan", "Yılmaz", 45),
+    Student("Berkay Reşat", "Bor", 77),
+    Student("Serkan", "Taşlıçay", 25)
+  ];
+
+  var studentss = [
+    "Onur Sercan Yılmaz",
     "Berkay Bor",
     "Serkan Taşlıçay",
-    "Enes",
-    "Mustafa",
-    "Kemal"
+    "Kemal Mehmet",
+    "Ali Şahin"
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(mesaj),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-                //farklı cihazlarda da güzel sonuçlar verir
-                child: ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return Text(students[index]);
-              },
-              itemCount: students.length,
-            )),
-            Center(
-              child: RaisedButton(
-                child: Text("Result"),
-                onPressed: () {
-                  int mark = 30;
-                  String message = "";
-                  if (mark >= 50) {
-                    message = "Passed";
-                  } else if (mark >= 40) {
-                    message = "make-up exam";
-                  } else {
-                    message = "Failed";
-                  }
-                  var alert = AlertDialog(
-                    title: Text("Exam Result"),
-                    content: Text(message),
-                  );
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) => alert);
-                  //context widget ağaç yapısındaki bilgiyi tutan yapıdır.
-                },
+      appBar: AppBar(
+        title: Text(mesaj),
+      ),
+      body: buildBody(context),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            //farklı cihazlarda da güzel sonuçlar verir
+            child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: Container(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://media-exp1.licdn.com/dms/image/C4D03AQEIY5a6tad9tA/profile-displayphoto-shrink_200_200/0/1601556741918?e=1614816000&v=beta&t=rfMqiWPgXtjTEBR3tB5BddTomB5fhf7YLR0nS_C_O74"),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ));
+              title: Text(
+                  students[index].firstName + " " + students[index].lastName),
+              subtitle: Text("Exam Result: " +
+                  students[index].grade.toString() +
+                  " [" +
+                  students[index].getStatus +
+                  "]"),
+              trailing: buildStatusIcon(students[index].grade),
+              onTap: () {},
+            );
+          },
+          itemCount: students.length,
+        )),
+        Center(
+          child: RaisedButton(
+            child: Text("Result"),
+            onPressed: () {
+              var mes = resultCalc(20);
+
+              showMessage(context, mes);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  String resultCalc(int mark) {
+    String message = "";
+    if (mark >= 50) {
+      message = "Passed";
+    } else if (mark >= 40) {
+      message = "make-up exam";
+    } else {
+      message = "Failed";
+    }
+    return message;
+  }
+
+  Widget buildStatusIcon(int grade) {
+    if (grade >= 50) {
+      return Icon(Icons.done);
+    } else if (grade >= 40) {
+      return Icon(Icons.album);
+    } else {
+      return Icon(Icons.clear);
+    }
   }
 }
 
@@ -74,3 +114,13 @@ class MyApp extends StatelessWidget {
                 ],
               ),
 * */
+
+void showMessage(BuildContext context, String message) {
+  var alert = AlertDialog(
+    title: Text("Exam Result"),
+    content: Text(message),
+  );
+
+  showDialog(context: context, builder: (BuildContext context) => alert);
+  //context widget ağaç yapısındaki bilgiyi tutan yapıdır.
+}
