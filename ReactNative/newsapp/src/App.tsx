7bbebd4c25f1c66dee788566713efa14cd513850
onursercanyilmaz <onursercanyilmaz@gmail.com>
2023-12-05 23:43:22 +0300

    stack navigation example

--- ReactNative/newsapp/src/App.tsx
+++ ReactNative/newsapp/src/App.tsx
@@ -1,52 +1,45 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, SafeAreaView, FlatList, ScrollView} from 'react-native';
-import axios from 'axios';
-import Card from './components/Cards/index';
-import {REACT_APP_NEWS_URL} from '../config'; // Import the environment variable
+// App.js
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(REACT_APP_NEWS_URL, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-          },
-        });
-        setData(response.data?.articles);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <SafeAreaView>
-      <View>
-        {data.length > 0 ? (
-          <>
-            <FlatList
-              ListHeaderComponent={() => (
-                <ScrollView horizontal>
-                  {data.map((item: any) => {
-                    return <Card key={item.title} data={item} />;
-                  })}
-                </ScrollView>
-              )}
-              data={data}
-              keyExtractor={(item: any) => item.title} // Use a unique key for each item, it is important for React to know which item has changed
-              renderItem={({item}) => <Card key={item.title} data={item} />} // Use your Card component and pass relevant props
-            />
-          </>
-        ) : (
-          <Text>Loading...</Text>
-        )}
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export function AboutScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>About Screen</Text>
+    </View>
+  );
+}
+
+// Homescreen.js
+import {Button} from 'react-native';
+
+type HomeScreenProps = {
+  navigation: any;
+};
+
+export function HomeScreen(props: HomeScreenProps) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to About"
+        onPress={() => props.navigation.navigate('About')}
+      />
+    </View>
   );
 }
