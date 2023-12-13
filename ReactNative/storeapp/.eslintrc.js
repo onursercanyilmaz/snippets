79eb67c5aeb2c58232ebb117ee8f82f9179b1fb1
onursercanyilmaz <onursercanyilmaz@gmail.com>
2023-12-13 22:26:35 +0300

    staged files

---
+++ ReactNative/storeapp/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', {args: 'none'}],
+  },
+};
